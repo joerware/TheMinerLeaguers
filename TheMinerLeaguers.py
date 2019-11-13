41a7cc67c95c2e312c681adf30c30e1c724341bc
joerware <55007806+joerware@users.noreply.github.com>
2019-11-12 19:14:12 -0500

    Correlation, zscores, etc.

--- TheMinerLeaguers.py
+++ TheMinerLeaguers.py
@@ -1,26 +1,34 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[91]:
 
 
 # Import necessary modules/libraries.
 import numpy as np
 import pandas as pd
 import pandas_profiling
+import matplotlib.pyplot as plt
 from datetime import datetime
 from scipy.stats import zscore
 from ast import literal_eval
 
 
-# In[ ]:
+# In[92]:
 
 
 #Define dataframe from local CSV file
 my_data = pd.read_csv('C:\\Users\\Joe\\Desktop\\tmdb_5000_movies.csv')
 
 
-# In[ ]:
+# In[93]:
+
+
+#Identify any potential duplicates by title; many remakes have the same title and should be retained.
+my_data[my_data.duplicated(['title'],keep=False)]
+
+
+# In[94]:
 
 
 # Generate new columns for cleaned attribute values; old values are retained for provenance.
@@ -30,7 +38,7 @@ my_data['production_companies_clean'] = 0
 my_data['roi'] = 0
 
 
-# In[ ]:
+# In[102]:
 
 
 #Must set new column as object type to allow insertion of lists into dataframes, otherwise Type error will occur.
@@ -39,10 +47,10 @@ my_data['keywords_clean'] = my_data['keywords_clean'].astype(object)
 my_data['production_companies_clean'] = my_data['production_companies_clean'].astype(object)
 my_data['budget'] = my_data['budget'].astype(float)
 my_data['revenue'] = my_data['revenue'].astype(float)
-my_data['roi'] = my_data['revenue'].astype(float)
+my_data['roi'] = my_data['roi'].astype(float)
 
 
-# In[ ]:
+# In[103]:
 
 
 # From genre attribute, this extracts values from 'name' keys as a list.
@@ -51,7 +59,7 @@ for i in range(len(my_data)):
     
 
 
-# In[ ]:
+# In[104]:
 
 
 # From keywords attribute, this extracts values from 'name' keys as a list.
@@ -60,7 +68,7 @@ for i in range(len(my_data)):
     
 
 
-# In[ ]:
+# In[105]:
 
 
 # From production_companies attribute, this extracts values from 'name' keys as a list.
@@ -69,7 +77,7 @@ for i in range(len(my_data)):
     
 
 
-# In[ ]:
+# In[106]:
 
 
 # Mark to delete rows that contain value of 0 for budget.
@@ -78,7 +86,7 @@ for i in range(len(my_data)):
         my_data.at[i, "delete"] = 'Delete'
 
 
-# In[ ]:
+# In[107]:
 
 
 # Mark to delete rows that contain value of 0 for revenue.
@@ -87,14 +95,14 @@ for i in range(len(my_data)):
         my_data.at[i, "delete"] = 'Delete'
 
 
-# In[ ]:
+# In[108]:
 
 
 # Remove homepage, language, overview attributes.
 my_data.drop(columns=['original_language', 'homepage', 'overview'])
 
 
-# In[ ]:
+# In[109]:
 
 
 # Delete all rows marked for deletion.
@@ -104,7 +112,7 @@ my_data.drop(my_data[my_data["delete"] == 'Delete'].index, inplace = True)
 my_data.reset_index(drop=True, inplace=True)
 
 
-# In[ ]:
+# In[110]:
 
 
 #Calculate return on investment a generate a new column.
@@ -112,7 +120,7 @@ for i in range(len(my_data)):
     my_data.at[i, "roi"] = round((my_data.at[i, "revenue"] - my_data.at[i, "budget"]) /  my_data.at[i, "budget"], 2)
 
 
-# In[ ]:
+# In[111]:
 
 
 #Generate separate a day of the year and year column.
@@ -126,15 +134,16 @@ for i in range(len(my_data)):
 
 
 #Create dummy variables; nominal/categorical variables must be converted for regression analysis.
-my_data["genres_clean"].str.join('|').str.get_dummies()
+genre_dummies = my_data["genres_clean"].str.join('|').str.get_dummies()
 
 
-# In[ ]:
+# In[112]:
 
 
 # Outlier analysis using z-score; adds columns to compute z-score for budget/revenue.
 my_data['revenue_zscore'] = my_data[['revenue']].apply(zscore)
 my_data['budget_zscore'] = my_data[['budget']].apply(zscore)
+my_data['roi_zscore'] = my_data[['roi']].apply(zscore)
 
 #Scatter plot with data standardized via z-score
 #my_data.plot.scatter(x='budget_zscore', y='revenue_zscore', c='DarkBlue')
@@ -165,6 +174,23 @@ my_data.plot.scatter(x='budget_normalized', y='revenue_normalized', c='DarkBlue'
 """
 
 
+# In[113]:
+
+
+# Analyze Spearman correlation coefficient for numerical attributes.
+corr = my_data.corr(method ='spearman')
+corr.style.background_gradient(cmap='coolwarm')
+
+
+# In[114]:
+
+
+# Analyze Spearman correlation coefficient for genres; 
+# this aids in determining if genres can be combined or removed and still retain variance.
+corr = genre_dummies.corr(method ='spearman')
+corr.style.background_gradient(cmap='coolwarm')
+
+
 # In[ ]:
 
 
@@ -177,4 +203,25 @@ my_data.head(25)
 
 #Save dataframe as CSV somewhere
 #my_data.to_csv('C:\\Users\\Joe\\Desktop\\my_data_scores.csv')
+
+
+# In[ ]:
+
+
+# Strong correlation between budget and revenue is apparent;few outliers with small budgets, but high revenue.
+my_data.plot.scatter(x='budget', y='revenue', loglog=True, legend=False)
+plt.show()
+
+
+# In[ ]:
+
+
+my_data.plot.scatter(x='revenue', y='roi', legend=False)
+plt.show()
+
+
+# In[ ]:
+
+
+my_data
 
